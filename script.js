4928da6da94a8babea1dcd526d30f32ba0433652
accio-garry <168645773+accio-garry@users.noreply.github.com>
2024-05-19 20:05:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,39 @@
-// complete this js code
-function Person(name, age) {}
 
-function Employee(name, age, jobTitle) {}
 
-// Do not change code below this line
-window.Person = Person;
-window.Employee = Employee;
+ class Person {
+ 	constructor(name, age) {
+		this.name=name;
+		this.age=age;	
+ 	}
+
+	 greet(){
+		 return `Hello, my name is ${this.name}, I am ${this.age} years old.`;
+	 }
+ }
+
+
+class Employee extends Person{
+
+	constructor(name, age, jobTitle){
+		super(name, age);
+		this.jobTitle = jobTitle;
+		
+	}
+
+
+	
+	 jobGreet(){
+		 return `Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.`;
+	 }
+ }
+
+let person = new Person("Alice", 25);
+console.log(person.greet());
+let employee = new Employee("Bob", 30, "Manager");
+console.log(employee.jobGreet());
+
+
+
+// // Do not change code below this line
+// window.Person = Person;
+// window.Employee = Employee;
